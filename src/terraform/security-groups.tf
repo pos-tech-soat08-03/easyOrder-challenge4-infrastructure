8497d14330310d310bf2d20ca570a0351f3bc6db
Marcio Saragiotto <marcio.saragiotto@gmail.com>
2025-02-09 18:38:14 -0300

    fix: ajustando fmt

--- src/terraform/security-groups.tf
+++ src/terraform/security-groups.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "gruposeguranca" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1" 
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
